4eb011384d4c1b37fb368490f7f6d56c88183dac
Daegil Kim <84318115+mong3125@users.noreply.github.com>
2023-02-20 22:56:08 +0900

    initial commit

---
+++ src/main/java/apptive/team1/exchangeLanguage/ExchangeLanguageApplication.java
@@ -0,0 +1,13 @@
+package apptive.team1.exchangeLanguage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExchangeLanguageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExchangeLanguageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/apptive/team1/exchangeLanguage/ExchangeLanguageApplicationTests.java
@@ -0,0 +1,13 @@
+package apptive.team1.exchangeLanguage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExchangeLanguageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
