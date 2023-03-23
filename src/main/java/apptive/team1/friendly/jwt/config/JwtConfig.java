01691a50c0651b0ded56e224e8c168008b6c2763
Daegil Kim <mong3125@naver.com>
2023-03-23 22:50:06 +0900

    refactor: accessToken 유효기간 연장
    refreshToken을 구현하고 다시 줄여야 함

--- src/main/java/apptive/team1/friendly/jwt/config/JwtConfig.java
+++ src/main/java/apptive/team1/friendly/jwt/config/JwtConfig.java
@@ -15,10 +15,11 @@ public class JwtConfig {
 
     /**
      * Access Token Provider
+     * refresh token을 구현하기전까지는 accessToken의 유효기간을 365일로 함
      */
     @Bean(name = "tokenProvider")
     public JwtTokenProvider tokenProvider() {
-        return new JwtTokenProvider(accessTokenSecret, accessTokenValidityInSeconds);
+        return new JwtTokenProvider(accessTokenSecret, accessTokenValidityInSeconds * 6 * 24 * 365);
     }
 
 }
