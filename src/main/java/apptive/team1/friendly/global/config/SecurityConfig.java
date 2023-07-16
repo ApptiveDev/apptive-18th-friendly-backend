1272b8f40ba3a45d9aa65aa991988fa5a8e9fe3d
kimminwook <kimmw2_@naver.com>
2023-07-16 11:55:40 +0900

    SecurityConfig 수정

--- src/main/java/apptive/team1/friendly/global/config/SecurityConfig.java
+++ src/main/java/apptive/team1/friendly/global/config/SecurityConfig.java
@@ -54,7 +54,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers("/api/google/login", "/api/google/signup").permitAll()
                 .antMatchers("/auth/social/GOOGLE", "/auth/social/KAKAO").permitAll()
                 .antMatchers("/auth/social/GOOGLE/callback", "/auth/social/KAKAO/callback").permitAll()
-                .antMatchers("/tourism/admin").permitAll()
                 .anyRequest().authenticated()
                 // jwt filter config
                 .and()
