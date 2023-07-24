0ece1cc9ed1f5c45c9ef72c784e1df03333c2858
kimminwook <kimmw2_@naver.com>
2023-07-24 17:52:04 +0900

    exception 메소드 추가

--- src/main/java/apptive/team1/friendly/domain/post/exception/InvalidHashTagException.java
+++ src/main/java/apptive/team1/friendly/domain/post/exception/InvalidHashTagException.java
@@ -1,7 +1,23 @@
 package apptive.team1.friendly.domain.post.exception;
 
 public class InvalidHashTagException extends RuntimeException{
+
+    public InvalidHashTagException() {
+    }
+
     public InvalidHashTagException(String message) {
         super(message);
+    }
+
+    public InvalidHashTagException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public InvalidHashTagException(Throwable cause) {
+        super(cause);
+    }
+
+    public InvalidHashTagException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
     }
 }
