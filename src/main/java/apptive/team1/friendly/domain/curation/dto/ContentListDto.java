65375c66a2fe06249b1b06408d310da07368e353
kimminwook <kimmw2_@naver.com>
2023-08-03 15:27:19 +0900

    contentListDto 대표 이미지만 보여지도록 수정

--- src/main/java/apptive/team1/friendly/domain/curation/dto/ContentListDto.java
+++ src/main/java/apptive/team1/friendly/domain/curation/dto/ContentListDto.java
@@ -4,30 +4,28 @@ import apptive.team1.friendly.domain.curation.entity.Content;
 import apptive.team1.friendly.domain.curation.entity.Image;
 import apptive.team1.friendly.global.common.s3.ImageDto;
 import lombok.Data;
-
 import java.util.ArrayList;
 import java.util.List;
 
 @Data
 public class ContentListDto {
-    public ContentListDto(Long contentId, List<Image> images) {
+    public ContentListDto(Long contentId, Image image) {
         this.contentId = contentId;
-        for (Image image : images) {
-            ImageDto imageDto = new ImageDto(image.getOriginalFileName(), image.getUploadFileName(),
-                    image.getUploadFilePath(), image.getUploadFileUrl());
-            this.images.add(imageDto);
-        }
+        this.image = new ImageDto(image.getOriginalFileName(), image.getUploadFileName(),
+                image.getUploadFilePath(), image.getUploadFileUrl());
     }
 
     private Long contentId;
 
-    private List<ImageDto> images = new ArrayList<>();
+    private ImageDto image;
+
+    //========정적 생성 메서드========//
     public static List<ContentListDto> create(List<Content> contents) {
 
         List<ContentListDto> contentListDtos = new ArrayList<>();
 
         for (Content content : contents) {
-            ContentListDto contentListDto = new ContentListDto(content.getId(), content.getImages());
+            ContentListDto contentListDto = new ContentListDto(content.getId(), content.getImages().get(0));
             contentListDtos.add(contentListDto);
         }
 
