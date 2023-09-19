4aba56654e25f491eacfd0e27acc2c6e83ef410d
k-kmw <kimmw2_@naver.com>
2023-09-19 22:00:54 +0900

    feat: 게시물 상세페이지에 참여자 이미지 추가

---
+++ src/main/java/apptive/team1/friendly/domain/post/vo/Participant.java
@@ -0,0 +1,19 @@
+package apptive.team1.friendly.domain.post.vo;
+
+import apptive.team1.friendly.global.common.s3.ImageDto;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class Participant {
+
+    public Participant(Long id, ImageDto profileImg) {
+        this.id = id;
+        this.profileImgDto = profileImg;
+    }
+
+    private Long id;
+
+    private ImageDto profileImgDto;
+
+}
