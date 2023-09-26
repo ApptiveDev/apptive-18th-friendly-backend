6a677e20a88b5c93f895aa3df643d66bbb4f97b5
mong3125 <mong3125@naver.com>
2023-09-26 17:27:39 +0900

    feat: 댓글 삭제 구현
    댓글의 저자인지 확인 후 삭제
    저자가 아니라면 403 error 반환

--- src/main/java/apptive/team1/friendly/domain/post/controller/comment/CommentController.java
+++ src/main/java/apptive/team1/friendly/domain/post/controller/comment/CommentController.java
@@ -2,10 +2,14 @@ package apptive.team1.friendly.domain.post.controller.comment;
 
 import apptive.team1.friendly.domain.post.dto.comment.CommentFormDto;
 import apptive.team1.friendly.domain.post.service.comment.CommentService;
+import apptive.team1.friendly.domain.user.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -13,6 +17,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 @Controller
 @RequiredArgsConstructor
 public class CommentController {
+    private final UserService userService;
     private final CommentService commentService;
     @PostMapping("/posts/{postId}/comments")
     public ResponseEntity<Long> addComment(@PathVariable(value = "postId") Long postId, @RequestBody CommentFormDto comment) {
@@ -20,4 +25,12 @@ public class CommentController {
         return new ResponseEntity<>(commentId, HttpStatus.OK);
     }
 
+    @DeleteMapping("/comment/{commentId}")
+    public ResponseEntity<?> deleteComment(@PathVariable(value = "commentId") Long commentId) {
+        Long currentUserId = userService.getCurrentUser().getId();
+
+        commentService.deleteComment(commentId, currentUserId);
+
+        return new ResponseEntity<>(null, HttpStatus.OK);
+    }
 }
